7351c30d7b174a06522a2669f4b1723697fb2892
olaf-stinnen-db <olaf.stinnen@arcor.de>
2021-06-27 17:05:08 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// document.getElementById("count").innerText = 5
+
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
